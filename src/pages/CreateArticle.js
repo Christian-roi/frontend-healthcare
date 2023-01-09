38108236a73be58578ca5052752634c38370fe1e
fathurrobb <ilhamfathurrobbani@gmail.com>
2023-01-10 00:08:17 +0700

    integrate backend on create article

--- src/pages/CreateArticle.js
+++ src/pages/CreateArticle.js
@@ -1,7 +1,11 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import { BsCamera } from "react-icons/bs";
+import ReactQuill from "react-quill";
+import 'react-quill/dist/quill.snow.css';
 import { useDispatch, useSelector } from "react-redux";
 import { Link, Navigate } from "react-router-dom";
 import Swal from "sweetalert2";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar"
 import { createPost } from "../redux/actions/post";
 
@@ -10,12 +14,19 @@ const CreateArticle = () => {
 
     const { message } = useSelector(state => state.message);
     const { user: currentUser } = useSelector((state) => state.auth);
+    const categories = useSelector(state => state.category);
+    const dataCategories = categories.data;
 
     const userId = currentUser.id;
     const [categoryId, setCategoryId] = useState('');
     const [title, setTitle] = useState('');
     const [image, setImage] = useState('');
     const [content, setContent] = useState('');
+
+    const hiddenFileInput = React.useRef(null);
+    const handleButtonClick = () => {
+        hiddenFileInput.current.click();
+    }
 
     const [imageEncode, setImageEncode] = useState({
         file: null,
@@ -58,7 +69,11 @@ const CreateArticle = () => {
             file: e.target.files[0]
         });
     };
-    
+
+    const removeSelectedImage = () => {
+        setImage('');
+    };
+
     const modules = {
         toolbar: [
           [{ 'header': [1, 2, false] }],
@@ -81,10 +96,11 @@ const CreateArticle = () => {
         setTitle(title);
     };
     
-    // const onChangeCategoryId  = e => {
-    //     const categoryId = e.target.value;
-    //     setPlace(categoryId);
-    // };
+    const onChangeCategoryId  = e => {
+        const categoryId = e.target.value;
+        // console.log("Ini ID nya", categoryId);
+        setCategoryId(categoryId);
+    };
     
     const [success, setSuccess] = useState(false);
     
@@ -128,7 +144,7 @@ const CreateArticle = () => {
                 <div className="container-fluid container-article">
                     <div className="row">
                         <div className="col-lg-4 mt-4">
-                            <h2>Article Choice</h2>
+                            <h2>Post an Article</h2>
                             <nav aria-label="breadcrumb">
                                 <ol className="breadcrumb navigate-menu">
                                     <li className="breadcrumb-item"><Link to={'/'}>Home</Link></li>
@@ -142,18 +158,61 @@ const CreateArticle = () => {
                         {
                             message && ( messageAlert() )
                         }
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-lg-4">
-
+                        <div className="container mt-4">
+                            <form onSubmit={handleSubmit}>
+                                <div className="row">
+                                    <div className="col-lg-4" >
+                                        {
+                                            image ?
+                                                <div className="preview">
+                                                    <img src={image} alt="preview cover"/>
+                                                    <div className="d-grip gap-1">
+                                                        <button className="btn btn-remove-preview mt-2" onClick={removeSelectedImage}>Remove this image</button>
+                                                    </div>
+                                                </div>    
+                                                :
+                                                <div className="btn-file-image" onClick={handleButtonClick}>
+                                                <BsCamera/>
+                                                    <h4 className="mx-auto mt-1" style={{marginBottom: '30%'}}>Drop your lovely Article Cover</h4>
+                                                    <input type="file" accept="image/*" id="image" name="image" ref={hiddenFileInput} onChange={handleFileInputChange} style={{display:'none'}} required/>
+                                                </div>
+                                        }
+                                    </div>
+                                    <div className="col-lg-8">
+                                        <input type="text" className="form-control" id="title" name="title" value={title} onChange={onChangeTitle} placeholder="Type in the title" required/>
+                                        <ReactQuill
+                                            className="col align-self-start mt-3"
+                                            style={{textAlign: 'left'}}
+                                            placeholder="Write down your insight here!"
+                                            theme='snow' 
+                                            modules={modules}
+                                            formats={formats}
+                                            value={content} 
+                                            onChange={setContent} 
+                                            required
+                                        />
+                                        <div className="row justify-content-end mt-5">
+                                            <div className="col-lg-4">                                                
+                                                <select onChange={onChangeCategoryId} className="form-select form-select-lg select-category">
+                                                    <option selected>Category</option>
+                                                    {
+                                                        dataCategories && dataCategories.map((category) => (
+                                                            <option value={category.id}>{category.name}</option>
+                                                        ))
+                                                    }
+                                                </select>
+                                            </div>
+                                            <div className="col-lg-2">
+                                                <button className='btn btn-lg btn-create-article'>Publish</button>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className="col-lh-8">
-
-                                </div>
-                            </div>
+                            </form>
                         </div>
                     </section>
                 </div>
+                <Footer/>
             </div>
         )
     }
--- src/pages/DetailArticle.js
+++ src/pages/DetailArticle.js
@@ -4,6 +4,7 @@ import { Link, useParams } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import postService from "../services/post";
 import blankProfile from "../assets/blank-profile.png";
+import Footer from "../components/Footer";
 
 const API_URL = 'http://localhost:8000/';
 const DetailArticle = () => {
@@ -54,12 +55,12 @@ const DetailArticle = () => {
                                 <div className="container">
                                     <div className="row">
                                         <div className="col-lg-12">
-                                            <img src={API_URL+currentPost.data.image} className="img-fluid" style={{width:'100%'}} />
+                                            <img src={API_URL+currentPost.data.image} className="img-fluid" style={{width:'100%'}} alt="cover article" />
                                         </div>
                                     </div>
                                     <div className="row justify-content-end mt-4 mb-4">
                                         <div className="col-lg-10 col-10" style={{textAlign:'left'}}>
-                                            <img src={blankProfile} className="rounded-circle float-start" width="50" height="50"/>
+                                            <img src={blankProfile} className="rounded-circle float-start" width="50" height="50" alt="avatar the writer"/>
                                             <div className="row">
                                                 <div className="col-lg-3 align-self-start">
                                                     <h5>{currentPost.data.user.first_name+" "+currentPost.data.user.last_name}</h5>
@@ -93,6 +94,7 @@ const DetailArticle = () => {
                     }
                 </div>
             </div>
+            <Footer/>
         </div>
     )
 };
